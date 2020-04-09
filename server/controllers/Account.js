f50b153d5b81a6c413bce40a5833141ee17ecdb9
Cindy Mills <sgm4689@g.rit.edu>
2020-04-09 15:52:04 -0400

    converted domomaker to react

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -6,13 +6,7 @@ const {
 
 const loginPage = (req, res) => {
   res.render('login', {
-    csrfToken: req.csrfToken()
-  });
-};
-
-const signupPage = (req, res) => {
-  res.render('signup', {
-    csrfToken: req.csrfToken()
+    csrfToken: req.csrfToken(),
   });
 };
 
@@ -105,8 +99,19 @@ const signup = (request, response) => {
   });
 };
 
+const getToken = (request, response) => {
+  const req = request;
+  const res = response;
+
+  const csrfJSON = {
+    csrfToken: req.csrfToken(),
+  };
+
+  res.json(csrfJSON);
+};
+
 module.exports.loginPage = loginPage;
 module.exports.login = login;
 module.exports.logout = logout;
-module.exports.signupPage = signupPage;
 module.exports.signup = signup;
+module.exports.getToken = getToken;
--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -1,7 +1,7 @@
 const models = require('../models');
 
 const {
-  Domo
+  Domo,
 } = models;
 
 const makerPage = (req, res) => {
@@ -14,7 +14,7 @@ const makerPage = (req, res) => {
     }
     return res.render('app', {
       csrfToken: req.csrfToken(),
-      domos: docs
+      domos: docs,
     });
   });
 };
@@ -55,5 +55,24 @@ const makeDomo = (req, res) => {
   return domoPromise;
 };
 
+const getDomos = (request, response) => {
+  const req = request;
+  const res = response;
+
+  return Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({
+        error: 'An error occured',
+      });
+    }
+
+    return res.json({
+      domos: docs,
+    });
+  });
+};
+
 module.exports.makerPage = makerPage;
 module.exports.make = makeDomo;
+module.exports.getDomos = getDomos;
